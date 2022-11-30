42ed056132eb29dea80cfc46fa021a43f3efde76
ogomez-at-wiris <oriol.gomez@wiris.com>
2022-11-30 08:15:51 +0100

    root commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+This is a Safari POC test for Selenium.
+
+It is already configured to run using Selenium safari webdriver.
+
+Steps.
+
+1. Install NPM on the machine if it is not installed [Node.js download page](https://nodejs.org/en/download/).
+2. Run `npm install` in the projdect directory.
+3. Run `npm start`.
+
+It should run Safari, start a Google search, show the results, and close. The console will say: "Found title".
---
+++ main.js
@@ -0,0 +1,32 @@
+const { webdriver, Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+const safari = require('selenium-webdriver/safari');
+
+const BROWSER = 'safari';
+
+let driver = new Builder()
+  .forBrowser(BROWSER)
+  .build();
+
+(async function example() {
+  try {
+    const version = require('selenium-webdriver').getVersion()
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('Wiris rulez', Key.RETURN);
+    await driver.wait(until.titleContains('Wiris'), 5000);
+    console.log('found title');
+
+/*
+    const slack = require('slack-notify')(process.env.SLACK_WEBHOOK_URL);
+    slack.send({
+      channel: '#qa-automation',
+      text: `Selenium POC test passed on browser: ${BROWSER} using version $browserVersion}`
+    });
+  });
+*/
+
+} finally {
+  await driver.quit();
+}
+}) ();
